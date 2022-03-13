cbdbcb7ab842533365fb72778e51469202ffbeea
TVandrus <T.Vandrus1@gmail.com>
2022-03-12 21:46:03 -0500

    updates

--- string-similarity.py
+++ string-similarity.py
@@ -8,98 +8,87 @@ Custom string fuzzy matching on a scale of [0, 1]
 Loosely based on Jaro similarity: https://en.wikipedia.org/wiki/Jaro–Winkler_distance
 Empirically, random/independent strings ~0.40 on average, similar strings >0.85
 """
-def string_compare(s1, s2; verbose=false, strip=[" "], keep_case=false, ignore_short=4)
+import math
 
+def string_compare(s1, s2, verbose=False, strip=[" "], keep_case=False, ignore_short=4):
     # Pre-Processing
     # remove spaces by default, allows arbitrary strings to be stripped away
-    if length(strip) > 0
-        for s in strip
-            s1, s2 = replace(s1, s => ""), replace(s2, s => "")
-        end
-    end
-    l1::Int, l2::Int = length(s1), length(s2)
-    if l1 < l2 # guarantee s1 is longest string
+    if len(strip) > 0:
+        for s in strip:
+            s1 = s1.replace(s, "")
+            s2 = s2.replace(s, "")
+    l1 = len(s1)
+    l2 = len(s2)
+    if l1 < l2: # guarantee s1 is longest string
         s1, s2 = s2, s1
         l1, l2 = l2, l1
-    end
-    if !keep_case # case insensitive by default
-        s1, s2 = uppercase(s1), uppercase(s2)
-    end
-    if verbose # display processed strings
-        println("$l1 - $s1")
-        println("$l2 - $s2")
-    end
-
-    # short circuit if above processing makes an exact match
-    if isequal(s1, s2)
-        return 1
-    end
     
-    if (l2 == 0) || (l2 <= ignore_short)
+    if not keep_case: # case insensitive by default
+        s1, s2 = s1.upper(), s2.upper()
+    
+    if verbose: # display processed strings
+        print(f"{l1} - {s1}")
+        print(f"{l2} - {s2}")
+    
+    # short circuit if above processing makes an exact match
+    if s1 == s2:
+        return 1
+    
+    if (l2 == 0) or (l2 <= ignore_short): 
         # arbitrary decision that fuzzy matching of 'short' strings
         #   is not informative
-        if verbose 
-            println("short string")
-        end
-        # already tested for exact matches
-        return 0
-    end
-
+        if verbose: 
+            print("short string")
+            # already tested for exact matches
+            return 0
     # matching-window size
     #   original uses (max length ÷ 2) - 1
     #mdist = (l1 ÷ 2) - 1
     #mdist = l1 ÷ 4 # bidirectional window needs to be smaller
-    mdist::Int = floor(Int, sqrt(l1))
-    if verbose
-        println("match dist - $mdist")
-    end
+    mdist = math.floor(math.sqrt(l1))
+    if verbose:
+        print(f"match dist - {mdist}")
     
     # order-sensitive match index of each character such that
     #   (goose, pot) has only one match [2], [2] but
     #   (goose, oolong) has two matches [1,2], [2,3]
-    m1::Array{Int}, m2::Array{Int} = [], []
+    m1, m2 = [], []
     # m1 needed only for debugging
-
-    for i::Int in 1:l1
-        window_start::Int = max(1, i-mdist)
-        window_end::Int = min(l2, i+mdist)
-        if window_start > l2
+    for i in range(0, l1): 
+        window_start = max(0, i-mdist)
+        window_end = min(l2, i+mdist)
+        if window_start > l2: 
             break
-        end
-        for j::Int in setdiff(window_start:window_end, m2)
-            if s1[i] == s2[j]
-                push!(m1, i) 
-                push!(m2, j)
+        for j in set(range(window_start, window_end)).difference(m2): 
+            if s1[i] == s2[j]: 
+                m1.append(i) 
+                m2.append(j) 
                 break
-            end
-        end            
-    end
-    if verbose
-        @debug m1 
-        @debug m2
-    end
-
-    matches::Int = length(m2)
-    if verbose
-        println("matches - $matches")
-    end
-
-    if matches == 0
+    if verbose:
+        print(m1) 
+        print(m2)
+    
+    matches = len(m2)
+    if verbose: 
+        print(f"matches - {matches}")
+    
+    if matches == 0: 
         return 0
-    elseif matches == 1
-        return round((1/l1 + 1/l2 + 1) / 3, digits=3)
-    else
-        transposes = sum([!isless(m2[k-1], m2[k]) for k in 2:matches])
-        if verbose
-            println("transposes - $transposes")
-        end
-        return round((matches / l1 + matches / l2 +
-                (matches - transposes) / matches ) / 3, digits=3)
-    end    
-end
+    elif matches == 1: 
+        return round((1/l1 + 1/l2 + 1) / 3, 3)
+    else: 
+        transposes = sum([(m2[k-1] >= m2[k]) for k in range(1, matches)])
+        if verbose:
+            print(f"transposes - {transposes}")
+        return round((matches / l1 + matches / l2 + (matches - transposes) / matches ) / 3, 3)
 
+s1 = "1313-123 Westcourt Place N2L 1B3"
+s2 = "Unit 1313 123 Westcourt Pl. N2L1B3"
 
-#= basic scenario testing
+string_compare(s1, s2, verbose=True)
+
+"""
+# basic scenario testing
 s1 = "martha"
 s2 = "marhta"
 
@@ -116,9 +105,10 @@ s1 = "123 Falconridge Cres Kitchener ON N2K1B3"
 s2 = "123 Falconridge Crescent Kitchener ON N2K1B3"
 
 string_compare(s1, s2, verbose=true) 
-=#
+"""
 
-#= performance testing
+"""
+# performance testing
 using Random, StatsBase, Base.Threads
 
 string_compare("test", "string", verbose=true)
@@ -137,4 +127,4 @@ summarystats(results)
 # performance test: 100k comparisons of random 100-char strings
 # original: 10s, 257M alloc, 15 GiB, 40% gc
 # type annotated: 9s 150M alloc, 13 GiB, 40% gc
-=#
+"""
