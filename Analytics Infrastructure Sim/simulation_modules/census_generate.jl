d38e6ea0dfcea7ac3c116754a436bdada0e043bd
TVandrus <T.Vandrus1@gmail.com>
2022-09-26 20:47:05 -0400

    updates

---
+++ Analytics Infrastructure Sim/simulation_modules/census_generate.jl
@@ -0,0 +1,20 @@
+
+using DataFrames 
+
+first_names = 
+last_names = 
+middle_inits = 
+
+
+function generate_population(n=100, id_seed=1000000, sources=[], configs=[])
+
+    entity_id
+    entity_type
+    full_name
+    f_name
+    l_name
+    mid_init 
+    dob 
+    address_id 
+    occup_id
+end 
