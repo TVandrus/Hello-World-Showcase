11071315ce329f10fb8e6995d7824be43c50abb9
TVandrus <T.Vandrus1@gmail.com>
2023-01-17 20:37:19 -0500

    adjusted simulation defaults, added comments

--- Evolutionary Learners/snake-evolution.jl
+++ Evolutionary Learners/snake-evolution.jl
@@ -1,54 +1,73 @@
 
 # snake-evolution.jl
 # teach an AI to play Snake (Neural Network trained via Evolutionary algorithm)
-
 module SnakeEvolution
-export run_simulation, sim_param, world_param, nnet, mutation
+export run_simulation, test_run, sim_param, world_param, nnet, mutation
 
 using DelimitedFiles
-using Base.Threads
-using CPUTime
-using Plots
+using Base.Threads, CPUTime
+using StatsBase, Plots 
 
 
 # structs and configurations 
 # define scenario 
+    const sim_param = (
+        max_gen=3_000, 
+        max_games=5, 
+        sneks=50, 
+        watch_gen=[0.5, 1, 1.5, 2, 3,4,6,9,13]*1000 # unused values are ignored 
+    )
+    const world_param = (rows=Int32(21), cols=Int32(21))
     #=
-    TODO: parameterize feedback (values affecting fitness) here
+    consider declining randomness for mutation aka simulated annealing
+    higher initial randomness (magnitude, or probability) for faster convergence
+    decreasing magnitude/probability later, based on time? or based on performance thresholds? 
+    to reduce major departures/'forgetfulness' after effective learning 
     =#
-    const sim_param = (max_gen=10_000, max_games=7, sneks=50, watch_gen=[1,2,5,9,10]*1000)
-    const world_param = (rows=Int32(19), cols=Int32(19))
-    const mutation = (prob_sm=0.30, max_amt_sm=1.0, prob_lg=0.25)
+    const mutation = (prob_sm=0.20, max_amt_sm=1.0, prob_lg=0.05) 
+    #=
+    Feedback events: 
+        Every tick in the game is a 'survived' event, minimal +ve reinforce 
+        Every step that reduces the horizontal or vertical distance from the head to the food is rewarded via 'approach'
+        Moving into the food gives the 'fed' moderate +ve reward
+        If the game ends by moving into an obstacle, then the 'crash' -ve reinforce is applied 
+        The game can go infinitely by looping a known path, accumulating survival or approach points 
+        To avoid the tail-chasing, a time limit is set as a failure condition
+        If the game ends via the time limit, the 'lazy' penalty is applied, more severe than the crash penalty
+        'idle_limit' steps are added to the cumulative time limit for each food collected
+        'achieve' reward can be added for diagnostic/comparative purposes
+    =#
     const feedback = (
         survive=1, # survival necessary, but not sufficient 
-        crash=-100, # non-survival severely penalized 
-        fed=25, # primary objective (requires survival) 
-        approach=1, # incentive to move toward food as default 
+        approach=0, # incentive to move toward food as default 
         avoid=0, # penalty for for not seeking food 
-        idle_limit=50, 
-        lazy=-300, # penalty for starving, survival without feeding is unproductive (ie chasing own tail infinitely)
-        achieve=10, # offset lazy penalty for points collected
+        fed=25, # primary objective (requires survival) 
+        crash=-200, # non-survival severely penalized 
+        achieve=0, 
+        idle_limit=75, 
+        lazy=-500, # penalty for 'starving'; survival without feeding is unproductive (ie chasing own tail infinitely)
     )
-
 
 # input model
     #=
-    TODO: define different models for drop-in replacements
+    Define different models for drop-in replacements
         absolute (up/down/left/right) vs relative directions (forward, turn left, turn right) 
+    
+    Inputs: 
+        up, right, down, left, relative position[r,c],
+        food dist [+/-, +/-], dir [+/-, +/-], memdir1..N[+/-, +/-]
     =#
-    # up, right, down, left, relative position[r,c],
-    #    food dist [+/-, +/-], dir [+/-, +/-], memdir1..N[+/-, +/-]
     const nnet_abs = (input=8,
-                hidden1=45,
-                hidden2=45,
-                hidden3=45,
+                hidden1=64,
+                hidden2=32,
+                hidden3=16,
                 output=4) # up, right, down, left
 
     const nnet_rel = (input=8,
-                hidden1=45,
-                hidden2=45,
-                hidden3=45,
-                output=3) # up, right, down, left
+                hidden1=64,
+                hidden2=32,
+                hidden3=16,
+                output=3) # left, straight, right 
 
     const nnet = nnet_abs # current architecture to be used 
 
@@ -132,8 +151,9 @@ using Plots
 
 ##################################################
 # core simulation components/logic 
+
 """
-Display a game State via terminal
+Display a game State via terminal  
 """
 function draw(state::State)::Nothing
 
@@ -154,97 +174,9 @@ function draw(state::State)::Nothing
 end
 
 """
-Updates Direction based on nnet logic applied to State 
+Updates Direction based on nnet logic applied to State  
 TODO: separate logic for different models (absolute vs relative)
 """
-function think!(state::State, brain::Brain)::State
-    # allocate model arrays
-    nn_input = zeros(Float32, 1, nnet.input)
-    nn_hidden1 = zeros(Float32, 1, nnet.hidden1)
-    nn_hidden2 = zeros(Float32, 1, nnet.hidden2)
-    nn_hidden3 = zeros(Float32, 1, nnet.hidden3)
-    nn_output = zeros(Float32, 1, nnet.output)
-    
-    # fill in inputs
-    # check adjacent squares: left, straight, and right
-    #=
-    for (i, adj) in enumerate([state.head+state.dir*R, state.head+state.dir, state.head-state.dir*R])
-        if any([ any(adj .< 1), (adj[1] > world_param.rows), (adj[2] > world_param.cols), (adj in state.body) ])
-            nn_input[i] = -1
-        elseif adj == state.food
-            nn_input[i] = 1
-        else # empty space
-            nn_input[i] = 0
-        end
-    end
-    =#
-    
-    #=
-    nn_input[9:12] = [state.head[1]/world_param.rows, 
-                    state.head[2]/world_param.cols,
-                    (state.food[1])/world_param.rows, 
-                    (state.food[2])/world_param.cols]
-
-    nn_input[9:10] = state.dir
-    nn_input[11:12] = state.mem1
-    nn_input[13:14] = state.mem2
-    nn_input[15:16] = state.mem3
-    
-    state.mem3 = state.mem2
-    state.mem2 = state.mem1
-    state.mem1 = state.dir
-    =#
-    
-    # fill in inputs
-    for (i, adj) in enumerate([d.up, d.right, d.down, d.left])
-        adj = state.head + adj
-        if all([ all(adj .> 0), (adj[1] <= world_param.rows), (adj[2] <= world_param.cols), !(adj in state.body) ])
-            nn_input[i] = 1 # safe to go
-        end
-    end
-    
-    fdir = state.head - state.food
-    if fdir[1] > 0
-        nn_input[5] = 1 # food is up
-    elseif fdir[1] < 0
-        nn_input[7] = 1 # food is down
-    end
-
-    if fdir[2] < 0
-        nn_input[6] = 1 # food is right
-    elseif fdir[2] > 0
-        nn_input[8] = 1 # food is left
-    end
-    
-    
-    # calculate output
-    nn_hidden1 = nn_input * brain.w_input_hidden1
-    nn_hidden1[nn_hidden1 .< 0] .= 0
-    nn_hidden2 = nn_hidden1 * brain.w_hidden1_hidden2
-    nn_hidden2[nn_hidden2 .< 0] .= 0
-    nn_hidden3 = nn_hidden2 * brain.w_hidden2_hidden3
-    nn_hidden3[nn_hidden3 .< 0] .= 0
-    nn_output = nn_hidden3 * brain.w_hidden3_output
-
-    a = argmax(vec(nn_output))
-    #a = rand([1,2,3,4]) #TESTING w RANDOM BRAIN
-
-    # update state direction accordingly
-    if a == 1 # up
-        state.dir = d.up
-    elseif a == 2 # right
-        state.dir = d.right
-    elseif a == 3 # down
-        state.dir = d.down
-    elseif a == 4 # left
-        state.dir = d.left
-    else
-        error("What are you trying to do?")
-    end
-
-    return state
-end
-
 function think_abs!(state::State, brain::Brain)::State
     # allocate model arrays
     nn_input = zeros(Float32, 1, nnet.input)
@@ -254,36 +186,6 @@ function think_abs!(state::State, brain::Brain)::State
     nn_output = zeros(Float32, 1, nnet.output)
     
     # fill in inputs
-    # check adjacent squares: left, straight, and right
-    #=
-    for (i, adj) in enumerate([state.head+state.dir*R, state.head+state.dir, state.head-state.dir*R])
-        if any([ any(adj .< 1), (adj[1] > world_param.rows), (adj[2] > world_param.cols), (adj in state.body) ])
-            nn_input[i] = -1
-        elseif adj == state.food
-            nn_input[i] = 1
-        else # empty space
-            nn_input[i] = 0
-        end
-    end
-    =#
-    
-    #=
-    nn_input[9:12] = [state.head[1]/world_param.rows, 
-                    state.head[2]/world_param.cols,
-                    (state.food[1])/world_param.rows, 
-                    (state.food[2])/world_param.cols]
-
-    nn_input[9:10] = state.dir
-    nn_input[11:12] = state.mem1
-    nn_input[13:14] = state.mem2
-    nn_input[15:16] = state.mem3
-    
-    state.mem3 = state.mem2
-    state.mem2 = state.mem1
-    state.mem1 = state.dir
-    =#
-    
-    # fill in inputs
     for (i, adj) in enumerate([d.up, d.right, d.down, d.left])
         adj = state.head + adj
         if all([ all(adj .> 0), (adj[1] <= world_param.rows), (adj[2] <= world_param.cols), !(adj in state.body) ])
@@ -304,7 +206,6 @@ function think_abs!(state::State, brain::Brain)::State
         nn_input[8] = 1 # food is left
     end
     
-    
     # calculate output
     nn_hidden1 = nn_input * brain.w_input_hidden1
     nn_hidden1[nn_hidden1 .< 0] .= 0
@@ -316,7 +217,7 @@ function think_abs!(state::State, brain::Brain)::State
 
     a = argmax(vec(nn_output))
     #a = rand([1,2,3,4]) #TESTING w RANDOM BRAIN
-
+    
     # update state direction accordingly
     if a == 1 # up
         state.dir = d.up
@@ -394,14 +295,12 @@ function think_rel!(state::State, brain::Brain)::State
     #a = rand([1,2,3,4]) #TESTING w RANDOM BRAIN
 
     # update state direction accordingly
-    if a == 1 # up
-        state.dir = d.up
-    elseif a == 2 # right
-        state.dir = d.right
-    elseif a == 3 # down
-        state.dir = d.down
-    elseif a == 4 # left
-        state.dir = d.left
+    if a == 1 # left
+        state.dir = state.dir * R 
+    elseif a == 2 # straight 
+        state.dir = state.dir 
+    elseif a == 3 # right
+        state.dir = state.dir * -R 
     else
         error("What are you trying to do?")
     end
@@ -410,9 +309,10 @@ function think_rel!(state::State, brain::Brain)::State
 end
 
 
+think! = think_abs! # used by run_game
 
 """
-Executes action determined by think!(), and evaluates consequences
+Executes action determined by think!(), and evaluates consequences  
 Produces a new State, including the reward calculation
 """
 function move!(state::State)::State
@@ -442,16 +342,15 @@ function move!(state::State)::State
         else # got food 
             state.points += 1 
             state.food = [rand(1:world_param.rows) rand(1:world_param.cols)]
-            state.fitness += feedback.fed * state.points
+            state.fitness += feedback.fed # multiply for escalating reward 
         end
         state.time += 1
     end
-
     return state
 end
 
 """
-Run a game start to finish
+Run a game start to finish  
 Return outcome as raw data 
 """
 function run_game(brain::Brain, watch::Bool)::Array{Int32, 1}
@@ -460,7 +359,7 @@ function run_game(brain::Brain, watch::Bool)::Array{Int32, 1}
         draw(state)
     end
     while ! state.crash 
-        state = think!(state, brain)
+        state = think!(state, brain) # replace think! to test other models
         state = move!(state)
         if watch
             draw(state)
@@ -479,7 +378,7 @@ function run_game(brain::Brain, watch::Bool)::Array{Int32, 1}
 end
 
 """
-Run all games for all sneks, then select and mutate
+Run all games for all sneks, then select and mutate  
 Return generational outcome and new generation
 """
 function run_generation(n_games::Int64, n_sneks::Int64, brains::Array{Brain, 1}, watch::Bool)::Tuple{Array{Float64, 1}, Array{Brain, 1}}
@@ -540,41 +439,43 @@ function run_generation(n_games::Int64, n_sneks::Int64, brains::Array{Brain, 1},
     if tester == clones[length(winners)+1]
         error("no mutation")
     end
-    return ( [ maximum(outcomes[:, 1]) / n_games,
+    return ( [ quantile(outcomes[:, 1], 0.80) / n_games,
             sum(outcomes[:, 1]) / n_games / n_sneks,
-            maximum(outcomes[:,2]) / n_games,
+            quantile(outcomes[:,2], 0.80) / n_games,
             sum(outcomes[:, 2]) / n_games / n_sneks,
-            maximum(outcomes[:,3]) / n_games,
+            quantile(outcomes[:,3], 0.80) / n_games, 
             sum(outcomes[:, 3]) / n_games / n_sneks ],
             clones)
 end
 
 """
-Run all generations
+Run all generations  
 Track execution and learning progress over time, present summary of final learning outcomes 
 """
 function run_simulation(n_generations=sim_param.max_gen, n_games=sim_param.max_games, n_sneks=sim_param.sneks, watch_gen=sim_param.watch_gen)
     brains = [Brain() for n in 1:n_sneks]
     
-    # max time, avg time, max points, avg points, max fitness, avg fitness, generation execution time
+    println("Running independent games on ", Threads.nthreads(), " threads")
+
+    # 80th percentile time, avg time, 80th percentile points, avg points, 80th percentile fitness, avg fitness, generation execution time
     gen_record = zeros(Float64, n_generations, 8)
     gen_record[:, 1] = 1:n_generations
-
+    
     for generation in 1:n_generations
         t = @elapsed gen_record[generation, 2:7], brains = run_generation(n_games, n_sneks, brains, (generation in watch_gen))
         gen_record[generation, 8] = t
 
         if generation % 50 == 0
             println("Generation ", generation, " completed in: ", round(gen_record[generation, 8], digits=4), 
-                    "\tMax fitness: ", round(gen_record[generation, 6], digits=1),
+                    "\t80th % fitness: ", round(gen_record[generation, 6], digits=1),
                     "\tAvg fitness: ", round(gen_record[generation, 7], digits=1))
         end
     end
     
     samples = 1:Int64(max(1, floor(n_generations/500))):n_generations
-    p1 = plot(gen_record[samples, 1], gen_record[samples, 2:3], seriestype=:scatter, xlim=:auto, ylim=[0,1000], title="Snek Time", label=["Max" "Avg"])
-    p2 = plot(gen_record[samples, 1], gen_record[samples, 4:5], seriestype=:scatter, xlim=:auto, ylim=[0,20], title="Snek Points", label=["Max" "Avg"])
-    p3 = plot(gen_record[samples, 1], gen_record[samples, 6:7], seriestype=:scatter, xlim=:auto, ylim=[-100,4000], title="Snek Fitness", label=["Max" "Avg"])
+    p1 = plot(gen_record[samples, 1], gen_record[samples, 2:3], seriestype=:scatter, xlim=:auto, ylim=[0,1000], title="Snek Time", label=["80th %" "Avg"])
+    p2 = plot(gen_record[samples, 1], gen_record[samples, 4:5], seriestype=:scatter, xlim=:auto, ylim=[0,25], title="Snek Points", label=["80th %" "Avg"])
+    p3 = plot(gen_record[samples, 1], gen_record[samples, 6:7], seriestype=:scatter, xlim=:auto, ylim=[-200,4000], title="Snek Fitness", label=["80th %" "Avg"])
 
     #display(plot(p2, p3, layout=(2,1)))
 
@@ -582,7 +483,7 @@ function run_simulation(n_generations=sim_param.max_gen, n_games=sim_param.max_g
 end
 
 """
-Trivial usage to pre-compile and demonstrate function
+Trivial usage to pre-compile and demonstrate function  
 """
 function test_run()
     return run_simulation(n_generations=2, n_games=1, n_sneks=5, watch_gen=[1])
