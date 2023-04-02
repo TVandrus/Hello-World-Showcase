b9e758d51cc28380b9744003f7a89a57eb9a68a7
TVandrus <T.Vandrus1@gmail.com>
2023-04-01 20:25:44 -0400

    reorganizing

--- Analytics Infrastructure Sim/execution_model/deployed_instance/quick_test.ps1
+++ Analytics Infrastructure Sim/execution_model/deployed_instance/quick_test.ps1
@@ -2,12 +2,6 @@
 cd 'S:/Datasets & Projects/LocalRepo/Sample-Projects/Analytics Infrastructure Sim/execution_model' 
 $env:DAGSTER_HOME = 'S:/Datasets & Projects/LocalRepo/Sample-Projects/Analytics Infrastructure Sim/execution_model/deployed_instance' 
 
-$dagster_daemon = 'S:/Python/Python_3_10/Scripts/dagster-daemon.exe' 
-cls; .$dagster_daemon run --python-file 'execution_patterns/ELT-graph.py' 
-
-# start dagit server 
-cd 'S:/Datasets & Projects/LocalRepo/Sample-Projects/Analytics Infrastructure Sim/execution_model' 
-$env:DAGSTER_HOME = 'S:/Datasets & Projects/LocalRepo/Sample-Projects/Analytics Infrastructure Sim/execution_model/deployed_instance' 
-
-$dagit = 'S:/Python/Python_3_10/Scripts/dagit.exe' 
-cls; .$dagit --python-file 'execution_patterns/ELT-graph.py' 
+# as of dagster v1.1.11, 'dagster dev' runs both dagit and daemon
+$dagster = 'S:/Python/Python_3_10/Scripts/dagster.exe' 
+cls; .$dagster dev --python-file 'execution_patterns/ELT-graph.py' 
