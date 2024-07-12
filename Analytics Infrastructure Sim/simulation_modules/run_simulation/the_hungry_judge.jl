ca1cffd901b943207fcc0e3c8899db072f6d43eb
TVandrus <T.Vandrus1@gmail.com>
2024-07-12 17:33:56 -0400

    misc ideas

--- Analytics Infrastructure Sim/simulation_modules/run_simulation/the_hungry_judge.jl
+++ Analytics Infrastructure Sim/simulation_modules/run_simulation/the_hungry_judge.jl
@@ -1,19 +1,48 @@
-using DataFrames, Dates, ProgressMeter, CSV
+
+using DataFrames, Dates, ProgressMeter, CSV, DuckDB
 
 n_txn = 1_000;
 txn_id0 = 6_000_000;
-menu = []
 period_start = Date(2020, 1, 1)
 period_end = Date(2020, 12, 30)
 MORNING = 7:11;
 MIDDAY = 11:4;
 EVENING = 4:9;
 
-SMALL_ORDER = (mu=5, sigma=3)
-LARGE_ORDER = (mu=15, sigma=10)
+SMALL_ORDER = (mu=5, sigma=3);
+LARGE_ORDER = (mu=15, sigma=10);
 
-menu = DataFrame(CSV.File("the_hungry_judge_menu.csv"));
+menu = DataFrame(CSV.File("Analytics Infrastructure Sim/simulation_modules/run_simulation/the_hungry_judge_menu.csv"));
+
+struct SaleTransaction
+    tx_id::Int 
+    tx_sub_id::Int
+    ux_id::Int 
+    pymt_id::Int
+    tx_date::Date 
+    tx_time::Time 
+    item_id::String 
+    item_price::Number 
+end
+
+
+function find_client()
+
+end
+
+
+function find_date()
+
+end 
+
+
+function find_order()
+
+end
 
 for t in 1:n_txn
-    
+    t_id = txn_id0 + t
+    ux_id = find_client()
+
+
 end
---
+++ Julia Implementations/chaotic_lorenz_simulation.jl
@@ -0,0 +1,484 @@
+### A Pluto.jl notebook ###
+# v0.19.43
+
+using Markdown
+using InteractiveUtils
+
+# ╔═╡ b5e41461-b495-4675-83ac-e21a0a98f5ca
+# imports
+using Unitful, DataFrames
+# units for calculations
+# dataframes to represent state
+# plots for visualizing state 
+# duckdb to record/playback a sequence
+
+# ╔═╡ c5887070-3fd2-11ef-2ca5-e187879f3427
+md"Simulation of complex systems that exhibit chaotic behaviours, making simulations entertaining to watch"
+
+# ╔═╡ aa475e33-0ed3-422a-ae0a-1e97d657df77
+md"# Lorenz Waterwheel"
+
+# ╔═╡ cdb95efa-1e5a-421e-98cf-1933ee7842ae
+md"
+[https://www.collimator.ai/tutorials/surrogate-model-of-a-chaotic-lorenz-waterwheel](https://www.collimator.ai/tutorials/surrogate-model-of-a-chaotic-lorenz-waterwheel)
+https://en.wikipedia.org/wiki/Torque
+https://en.wikipedia.org/wiki/Malkus_waterwheel
+
+https://en.wikipedia.org/wiki/Lorenz_system#Julia_simulation
+"
+
+# ╔═╡ 3e9f3839-40ea-4f88-a81b-f2c87218b1a5
+md"## System Mechanics"
+
+# ╔═╡ 31b046cf-a232-48be-aa25-e27f4c85ad71
+struct State
+	𝑡
+	θ
+	ω
+	buckets
+end
+
+# ╔═╡ f949dc42-bc2e-406d-8247-714f0b3c561b
+md"# Initialize System Properties"
+
+# ╔═╡ bda19153-039d-4541-a06e-116688abe810
+
+begin
+	𝑡_step = 0.01u"s" # temporal resolution of the simulation
+	
+	g⃗ = 9.8u"m/s^2"
+	inflow_rate = 0.2u"kg/s"
+	max_fill = 1u"kg"
+	outflow_rate = 0.05u"kg/s"
+	
+	n_buckets = 11
+	N = n_buckets
+	wheel_radius = 1u"m"
+	wheel_inertia = 5u"kg*m^2"
+	ν = 0.5u"N*m" # braking torque
+end
+
+# ╔═╡ 7d2fcbe4-ac26-45d9-b13a-a6d207bc3998
+function step(state, fill_rate, drain_rate, step_time)
+	B′ = DataFrame()
+	B′.id = state.buckets.id
+	# update bucket mass
+	fill_position = (-0.05 .< state.buckets.pos_x .< 0.05) && (state.buckets.pos_y .> 0)
+	B′.mass = (
+		state.buckets.mass 
+		# if a bucket is in a position to be filled, increase its mass for one step
+		.+ (inflow_rate*step_time) .* fill_position
+		# every bucket that has a filled mass loses some for one step
+		# basic implementation: all buckets constant drain rate
+		# realistic: each bucket drains at a rate according to its filled mass and shape of water for one step
+		.- (drain_rate*step_time) 
+	)
+
+	# calculate observable state
+	B′.pos_θ = B′.id/N .* π .* 2u"rad"
+	B′.pos_x = sin.(B′.pos_θ) * wheel_radius
+	B′.pos_y = cos.(B′.pos_θ) * wheel_radius
+	# update resultant dynamics
+	B′.τ = B′.mass .* sin.(B′.pos_θ) .* wheel_radius * g⃗
+	ω′ = state.ω + sum(B′.τ) * step_time
+	θ′ = state.θ + (state.ω * step_time)
+	𝑡′ = state.𝑡 + step_time 
+	
+	return State
+end
+
+# ╔═╡ c821788a-ea86-47c8-bf55-e086d5ebfb95
+function initialize()
+	return State(
+		0u"s",
+		0u"rad",
+		π/100u"rad/s",
+		DataFrame(
+			:id=>1:N, 
+			:mass=>zeros(N),
+		)
+	)
+end
+
+# ╔═╡ 8205621a-7fe8-4115-9642-214ec377c8cd
+function play_simulation(time_limit=100u"s")
+	curr_state = initialize()
+	while curr_state.𝑡 < time_limit
+		curr_state = step(curr_state)
+	end
+end
+
+# ╔═╡ 911084f1-27df-4fc5-9942-9c3092d30791
+begin
+	s_0 = 
+end
+
+# ╔═╡ 59a69a18-f417-4b22-bd44-86d450ecf95d
+state = s_0;
+
+# ╔═╡ e49a4c8c-486f-436a-a78a-1bd826c7faea
+B′ = DataFrame();
+
+# ╔═╡ beb0e29b-1325-4873-baa6-a19e245d8669
+τ_i = -0.5u"kg" * sin(π/2u"rad") * wheel_radius * g⃗ * 0.01u"s"
+
+# ╔═╡ b5d79250-4d4d-48bb-bfc6-66f402abec29
+# interactive display
+
+# ╔═╡ 00000000-0000-0000-0000-000000000001
+PLUTO_PROJECT_TOML_CONTENTS = """
+[deps]
+DataFrames = "a93c6f00-e57d-5684-b7b6-d8193f3e46c0"
+Unitful = "1986cc42-f94f-5a68-af5c-568840ba703d"
+
+[compat]
+DataFrames = "~1.6.1"
+Unitful = "~1.20.0"
+"""
+
+# ╔═╡ 00000000-0000-0000-0000-000000000002
+PLUTO_MANIFEST_TOML_CONTENTS = """
+# This file is machine-generated - editing it directly is not advised
+
+julia_version = "1.9.3"
+manifest_format = "2.0"
+project_hash = "f981d9cbdc8052033dd96cd7709ef10f3c3ed50b"
+
+[[deps.ArgTools]]
+uuid = "0dad84c5-d112-42e6-8d28-ef12dabb789f"
+version = "1.1.1"
+
+[[deps.Artifacts]]
+uuid = "56f22d72-fd6d-98f1-02f0-08ddc0907c33"
+
+[[deps.Base64]]
+uuid = "2a0f44e3-6c83-55bd-87e4-b1978d98bd5f"
+
+[[deps.Compat]]
+deps = ["TOML", "UUIDs"]
+git-tree-sha1 = "b1c55339b7c6c350ee89f2c1604299660525b248"
+uuid = "34da2185-b29b-5c13-b0c7-acf172513d20"
+version = "4.15.0"
+weakdeps = ["Dates", "LinearAlgebra"]
+
+    [deps.Compat.extensions]
+    CompatLinearAlgebraExt = "LinearAlgebra"
+
+[[deps.CompilerSupportLibraries_jll]]
+deps = ["Artifacts", "Libdl"]
+uuid = "e66e0078-7015-5450-92f7-15fbd957f2ae"
+version = "1.0.5+0"
+
+[[deps.Crayons]]
+git-tree-sha1 = "249fe38abf76d48563e2f4556bebd215aa317e15"
+uuid = "a8cc5b0e-0ffa-5ad4-8c14-923d3ee1735f"
+version = "4.1.1"
+
+[[deps.DataAPI]]
+git-tree-sha1 = "abe83f3a2f1b857aac70ef8b269080af17764bbe"
+uuid = "9a962f9c-6df0-11e9-0e5d-c546b8b5ee8a"
+version = "1.16.0"
+
+[[deps.DataFrames]]
+deps = ["Compat", "DataAPI", "DataStructures", "Future", "InlineStrings", "InvertedIndices", "IteratorInterfaceExtensions", "LinearAlgebra", "Markdown", "Missings", "PooledArrays", "PrecompileTools", "PrettyTables", "Printf", "REPL", "Random", "Reexport", "SentinelArrays", "SortingAlgorithms", "Statistics", "TableTraits", "Tables", "Unicode"]
+git-tree-sha1 = "04c738083f29f86e62c8afc341f0967d8717bdb8"
+uuid = "a93c6f00-e57d-5684-b7b6-d8193f3e46c0"
+version = "1.6.1"
+
+[[deps.DataStructures]]
+deps = ["Compat", "InteractiveUtils", "OrderedCollections"]
+git-tree-sha1 = "1d0a14036acb104d9e89698bd408f63ab58cdc82"
+uuid = "864edb3b-99cc-5e75-8d2d-829cb0a9cfe8"
+version = "0.18.20"
+
+[[deps.DataValueInterfaces]]
+git-tree-sha1 = "bfc1187b79289637fa0ef6d4436ebdfe6905cbd6"
+uuid = "e2d170a0-9d28-54be-80f0-106bbe20a464"
+version = "1.0.0"
+
+[[deps.Dates]]
+deps = ["Printf"]
+uuid = "ade2ca70-3891-5945-98fb-dc099432e06a"
+
+[[deps.Downloads]]
+deps = ["ArgTools", "FileWatching", "LibCURL", "NetworkOptions"]
+uuid = "f43a241f-c20a-4ad4-852c-f6b1247861c6"
+version = "1.6.0"
+
+[[deps.FileWatching]]
+uuid = "7b1f6079-737a-58dc-b8bc-7a2ca5c1b5ee"
+
+[[deps.Future]]
+deps = ["Random"]
+uuid = "9fa8497b-333b-5362-9e8d-4d0656e87820"
+
+[[deps.InlineStrings]]
+deps = ["Parsers"]
+git-tree-sha1 = "86356004f30f8e737eff143d57d41bd580e437aa"
+uuid = "842dd82b-1e85-43dc-bf29-5d0ee9dffc48"
+version = "1.4.1"
+
+    [deps.InlineStrings.extensions]
+    ArrowTypesExt = "ArrowTypes"
+
+    [deps.InlineStrings.weakdeps]
+    ArrowTypes = "31f734f8-188a-4ce0-8406-c8a06bd891cd"
+
+[[deps.InteractiveUtils]]
+deps = ["Markdown"]
+uuid = "b77e0a4c-d291-57a0-90e8-8db25a27a240"
+
+[[deps.InvertedIndices]]
+git-tree-sha1 = "0dc7b50b8d436461be01300fd8cd45aa0274b038"
+uuid = "41ab1584-1d38-5bbf-9106-f11c6c58b48f"
+version = "1.3.0"
+
+[[deps.IteratorInterfaceExtensions]]
+git-tree-sha1 = "a3f24677c21f5bbe9d2a714f95dcd58337fb2856"
+uuid = "82899510-4779-5014-852e-03e436cf321d"
+version = "1.0.0"
+
+[[deps.LaTeXStrings]]
+git-tree-sha1 = "50901ebc375ed41dbf8058da26f9de442febbbec"
+uuid = "b964fa9f-0449-5b57-a5c2-d3ea65f4040f"
+version = "1.3.1"
+
+[[deps.LibCURL]]
+deps = ["LibCURL_jll", "MozillaCACerts_jll"]
+uuid = "b27032c2-a3e7-50c8-80cd-2d36dbcbfd21"
+version = "0.6.3"
+
+[[deps.LibCURL_jll]]
+deps = ["Artifacts", "LibSSH2_jll", "Libdl", "MbedTLS_jll", "Zlib_jll", "nghttp2_jll"]
+uuid = "deac9b47-8bc7-5906-a0fe-35ac56dc84c0"
+version = "7.84.0+0"
+
+[[deps.LibGit2]]
+deps = ["Base64", "NetworkOptions", "Printf", "SHA"]
+uuid = "76f85450-5226-5b5a-8eaa-529ad045b433"
+
+[[deps.LibSSH2_jll]]
+deps = ["Artifacts", "Libdl", "MbedTLS_jll"]
+uuid = "29816b5a-b9ab-546f-933c-edad1886dfa8"
+version = "1.10.2+0"
+
+[[deps.Libdl]]
+uuid = "8f399da3-3557-5675-b5ff-fb832c97cbdb"
+
+[[deps.LinearAlgebra]]
+deps = ["Libdl", "OpenBLAS_jll", "libblastrampoline_jll"]
+uuid = "37e2e46d-f89d-539d-b4ee-838fcccc9c8e"
+
+[[deps.Logging]]
+uuid = "56ddb016-857b-54e1-b83d-db4d58db5568"
+
+[[deps.Markdown]]
+deps = ["Base64"]
+uuid = "d6f4376e-aef5-505a-96c1-9c027394607a"
+
+[[deps.MbedTLS_jll]]
+deps = ["Artifacts", "Libdl"]
+uuid = "c8ffd9c3-330d-5841-b78e-0817d7145fa1"
+version = "2.28.2+0"
+
+[[deps.Missings]]
+deps = ["DataAPI"]
+git-tree-sha1 = "ec4f7fbeab05d7747bdf98eb74d130a2a2ed298d"
+uuid = "e1d29d7a-bbdc-5cf2-9ac0-f12de2c33e28"
+version = "1.2.0"
+
+[[deps.MozillaCACerts_jll]]
+uuid = "14a3606d-f60d-562e-9121-12d972cd8159"
+version = "2022.10.11"
+
+[[deps.NetworkOptions]]
+uuid = "ca575930-c2e3-43a9-ace4-1e988b2c1908"
+version = "1.2.0"
+
+[[deps.OpenBLAS_jll]]
+deps = ["Artifacts", "CompilerSupportLibraries_jll", "Libdl"]
+uuid = "4536629a-c528-5b80-bd46-f80d51c5b363"
+version = "0.3.21+4"
+
+[[deps.OrderedCollections]]
+git-tree-sha1 = "dfdf5519f235516220579f949664f1bf44e741c5"
+uuid = "bac558e1-5e72-5ebc-8fee-abe8a469f55d"
+version = "1.6.3"
+
+[[deps.Parsers]]
+deps = ["Dates", "PrecompileTools", "UUIDs"]
+git-tree-sha1 = "8489905bcdbcfac64d1daa51ca07c0d8f0283821"
+uuid = "69de0a69-1ddd-5017-9359-2bf0b02dc9f0"
+version = "2.8.1"
+
+[[deps.Pkg]]
+deps = ["Artifacts", "Dates", "Downloads", "FileWatching", "LibGit2", "Libdl", "Logging", "Markdown", "Printf", "REPL", "Random", "SHA", "Serialization", "TOML", "Tar", "UUIDs", "p7zip_jll"]
+uuid = "44cfe95a-1eb2-52ea-b672-e2afdf69b78f"
+version = "1.9.2"
+
+[[deps.PooledArrays]]
+deps = ["DataAPI", "Future"]
+git-tree-sha1 = "36d8b4b899628fb92c2749eb488d884a926614d3"
+uuid = "2dfb63ee-cc39-5dd5-95bd-886bf059d720"
+version = "1.4.3"
+
+[[deps.PrecompileTools]]
+deps = ["Preferences"]
+git-tree-sha1 = "5aa36f7049a63a1528fe8f7c3f2113413ffd4e1f"
+uuid = "aea7be01-6a6a-4083-8856-8a6e6704d82a"
+version = "1.2.1"
+
+[[deps.Preferences]]
+deps = ["TOML"]
+git-tree-sha1 = "9306f6085165d270f7e3db02af26a400d580f5c6"
+uuid = "21216c6a-2e73-6563-6e65-726566657250"
+version = "1.4.3"
+
+[[deps.PrettyTables]]
+deps = ["Crayons", "LaTeXStrings", "Markdown", "PrecompileTools", "Printf", "Reexport", "StringManipulation", "Tables"]
+git-tree-sha1 = "66b20dd35966a748321d3b2537c4584cf40387c7"
+uuid = "08abe8d2-0d0c-5749-adfa-8a2ac140af0d"
+version = "2.3.2"
+
+[[deps.Printf]]
+deps = ["Unicode"]
+uuid = "de0858da-6303-5e67-8744-51eddeeeb8d7"
+
+[[deps.REPL]]
+deps = ["InteractiveUtils", "Markdown", "Sockets", "Unicode"]
+uuid = "3fa0cd96-eef1-5676-8a61-b3b8758bbffb"
+
+[[deps.Random]]
+deps = ["SHA", "Serialization"]
+uuid = "9a3f8284-a2c9-5f02-9a11-845980a1fd5c"
+
+[[deps.Reexport]]
+git-tree-sha1 = "45e428421666073eab6f2da5c9d310d99bb12f9b"
+uuid = "189a3867-3050-52da-a836-e630ba90ab69"
+version = "1.2.2"
+
+[[deps.SHA]]
+uuid = "ea8e919c-243c-51af-8825-aaa63cd721ce"
+version = "0.7.0"
+
+[[deps.SentinelArrays]]
+deps = ["Dates", "Random"]
+git-tree-sha1 = "ff11acffdb082493657550959d4feb4b6149e73a"
+uuid = "91c51154-3ec4-41a3-a24f-3f23e20d615c"
+version = "1.4.5"
+
+[[deps.Serialization]]
+uuid = "9e88b42a-f829-5b0c-bbe9-9e923198166b"
+
+[[deps.Sockets]]
+uuid = "6462fe0b-24de-5631-8697-dd941f90decc"
+
+[[deps.SortingAlgorithms]]
+deps = ["DataStructures"]
+git-tree-sha1 = "66e0a8e672a0bdfca2c3f5937efb8538b9ddc085"
+uuid = "a2af1166-a08f-5f64-846c-94a0d3cef48c"
+version = "1.2.1"
+
+[[deps.SparseArrays]]
+deps = ["Libdl", "LinearAlgebra", "Random", "Serialization", "SuiteSparse_jll"]
+uuid = "2f01184e-e22b-5df5-ae63-d93ebab69eaf"
+
+[[deps.Statistics]]
+deps = ["LinearAlgebra", "SparseArrays"]
+uuid = "10745b16-79ce-11e8-11f9-7d13ad32a3b2"
+version = "1.9.0"
+
+[[deps.StringManipulation]]
+deps = ["PrecompileTools"]
+git-tree-sha1 = "a04cabe79c5f01f4d723cc6704070ada0b9d46d5"
+uuid = "892a3eda-7b42-436c-8928-eab12a02cf0e"
+version = "0.3.4"
+
+[[deps.SuiteSparse_jll]]
+deps = ["Artifacts", "Libdl", "Pkg", "libblastrampoline_jll"]
+uuid = "bea87d4a-7f5b-5778-9afe-8cc45184846c"
+version = "5.10.1+6"
+
+[[deps.TOML]]
+deps = ["Dates"]
+uuid = "fa267f1f-6049-4f14-aa54-33bafae1ed76"
+version = "1.0.3"
+
+[[deps.TableTraits]]
+deps = ["IteratorInterfaceExtensions"]
+git-tree-sha1 = "c06b2f539df1c6efa794486abfb6ed2022561a39"
+uuid = "3783bdb8-4a98-5b6b-af9a-565f29a5fe9c"
+version = "1.0.1"
+
+[[deps.Tables]]
+deps = ["DataAPI", "DataValueInterfaces", "IteratorInterfaceExtensions", "LinearAlgebra", "OrderedCollections", "TableTraits"]
+git-tree-sha1 = "cb76cf677714c095e535e3501ac7954732aeea2d"
+uuid = "bd369af6-aec1-5ad0-b16a-f7cc5008161c"
+version = "1.11.1"
+
+[[deps.Tar]]
+deps = ["ArgTools", "SHA"]
+uuid = "a4e569a6-e804-4fa4-b0f3-eef7a1d5b13e"
+version = "1.10.0"
+
+[[deps.UUIDs]]
+deps = ["Random", "SHA"]
+uuid = "cf7118a7-6976-5b1a-9a39-7adc72f591a4"
+
+[[deps.Unicode]]
+uuid = "4ec0a83e-493e-50e2-b9ac-8f72acf5a8f5"
+
+[[deps.Unitful]]
+deps = ["Dates", "LinearAlgebra", "Random"]
+git-tree-sha1 = "dd260903fdabea27d9b6021689b3cd5401a57748"
+uuid = "1986cc42-f94f-5a68-af5c-568840ba703d"
+version = "1.20.0"
+
+    [deps.Unitful.extensions]
+    ConstructionBaseUnitfulExt = "ConstructionBase"
+    InverseFunctionsUnitfulExt = "InverseFunctions"
+
+    [deps.Unitful.weakdeps]
+    ConstructionBase = "187b0558-2788-49d3-abe0-74a17ed4e7c9"
+    InverseFunctions = "3587e190-3f89-42d0-90ee-14403ec27112"
+
+[[deps.Zlib_jll]]
+deps = ["Libdl"]
+uuid = "83775a58-1f1d-513f-b197-d71354ab007a"
+version = "1.2.13+0"
+
+[[deps.libblastrampoline_jll]]
+deps = ["Artifacts", "Libdl"]
+uuid = "8e850b90-86db-534c-a0d3-1478176c7d93"
+version = "5.8.0+0"
+
+[[deps.nghttp2_jll]]
+deps = ["Artifacts", "Libdl"]
+uuid = "8e850ede-7688-5339-a07c-302acd2aaf8d"
+version = "1.48.0+0"
+
+[[deps.p7zip_jll]]
+deps = ["Artifacts", "Libdl"]
+uuid = "3f19e933-33d8-53b3-aaab-bd5110c3b7a0"
+version = "17.4.0+0"
+"""
+
+# ╔═╡ Cell order:
+# ╟─c5887070-3fd2-11ef-2ca5-e187879f3427
+# ╟─aa475e33-0ed3-422a-ae0a-1e97d657df77
+# ╠═cdb95efa-1e5a-421e-98cf-1933ee7842ae
+# ╠═b5e41461-b495-4675-83ac-e21a0a98f5ca
+# ╠═3e9f3839-40ea-4f88-a81b-f2c87218b1a5
+# ╠═31b046cf-a232-48be-aa25-e27f4c85ad71
+# ╠═7d2fcbe4-ac26-45d9-b13a-a6d207bc3998
+# ╠═c821788a-ea86-47c8-bf55-e086d5ebfb95
+# ╠═8205621a-7fe8-4115-9642-214ec377c8cd
+# ╟─f949dc42-bc2e-406d-8247-714f0b3c561b
+# ╠═bda19153-039d-4541-a06e-116688abe810
+# ╠═911084f1-27df-4fc5-9942-9c3092d30791
+# ╠═59a69a18-f417-4b22-bd44-86d450ecf95d
+# ╠═e49a4c8c-486f-436a-a78a-1bd826c7faea
+# ╠═beb0e29b-1325-4873-baa6-a19e245d8669
+# ╠═b5d79250-4d4d-48bb-bfc6-66f402abec29
+# ╟─00000000-0000-0000-0000-000000000001
+# ╟─00000000-0000-0000-0000-000000000002
